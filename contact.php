df0e0967e7f0bad330ae28c5ee83075d781fc023
cheddy501 <libbelmopan@yahoo.com>
2025-07-23 14:07:15 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "libbelmopan@yahoo.com";
+    $subject = "New Contact Form Submission - C&J Estate Planning";
+
+    // Sanitize inputs
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $matter = strip_tags(trim($_POST["matter"]));
+    $message = strip_tags(trim($_POST["message"]));
+
+    // Validate email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = "Invalid email address.";
+    } elseif (empty($name) || empty($matter) || empty($message)) {
+        $error = "Please fill in all required fields.";
+    } else {
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n";
+        $email_content .= "Type of Matter: $matter\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        $headers = "From: $name <$email>\r\n";
+        $headers .= "Reply-To: $email\r\n";
+
+        if (mail($to, $subject, $email_content, $headers)) {
+            $success = "Thank you for contacting us, $name. We will get back to you shortly.";
+        } else {
+            $error = "Sorry, your message could not be sent. Please try again later.";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
