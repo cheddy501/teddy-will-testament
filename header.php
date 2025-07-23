cca4f9d6eca584d6ad92a5e2950c1e75cdbe2a8e
cheddy501 <libbelmopan@yahoo.com>
2025-07-23 13:55:10 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<header style="text-align: center; padding: 20px 0;">
+  <a href="index.html">
+    <img src="images/logo2.png" alt="C&J Estate Planning Logo" class="logo" />
+  </a>
+  <div>
+    <h1>C&J Estate Planning Consultancy</h1>
+    <p>Trustworthy Will & Estate Planning Services</p>
+  </div>
+</header>
+
+<nav>
+  <a href="index.html">Home</a>
+  <a href="about.html">About</a>
+  <div class="dropdown">
+    <a href="#">Services</a>
+    <div class="dropdown-content">
+      <a href="will-preparation.html">Last Will & Testament Preparation</a>
+      <a href="administration-preparation.html">Grant of Administration Preparation</a>
+      <a href="probate-preparation.html">Grant of Probate Preparation</a>
+      <a href="poa-financial.html">POA for Financial & Legal Matters</a>
+      <a href="poa-healthcare.html">POA for Healthcare & Personal Care</a>
+      <a href="trusts.html">Trusts</a>
+    </div>
+  </div>
+  <a href="contact.html">Contact</a>
+  <a href="faq.html">FAQ</a>
+</nav>
